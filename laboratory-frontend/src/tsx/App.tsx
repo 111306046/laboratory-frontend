2bfd152c5f9faa7e9be41861519d66890b88358a
111306046 <111306046@nccu.edu.tw>
2025-04-14 16:15:59 +0800

    Update App.tsx

--- laboratory-frontend/src/tsx/App.tsx
+++ laboratory-frontend/src/tsx/App.tsx
@@ -1,12 +1,15 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<Register />} />
-        
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );

8852a27be1be876555efde6569f303058642d53a
111306046 <111306046@nccu.edu.tw>
2025-04-14 16:09:44 +0800

    login page

--- laboratory-frontend/src/tsx/App.tsx
+++ laboratory-frontend/src/tsx/App.tsx
@@ -1,11 +1,18 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<Register />} />
+<<<<<<< Updated upstream
+=======
+        <Route path="/login" element={<Login />} />
+>>>>>>> Stashed changes
       </Routes>
     </BrowserRouter>
   );
